ebb63a7bea48c7593ccd46bf16ab084880a9f859
Joeeazy <joeeazy34@gmail.com>
2024-05-01 12:39:54 +0300

    added products and filter by category implementation done

--- src/pages/home/Products.jsx
+++ src/pages/home/Products.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import { FaFilter } from "react-icons/fa";
+import Cards from "../../components/Cards";
 
 export default function Products() {
+  const [products, setProducts] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [sortOptions, setSortOptions] = useState("default");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/images/products.json"); //fetch package.json
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`); //check if res was ok
+        }
+        const data = await response.json();
+        setProducts(data);
+        //console.log(products);
+        setFilteredItems(data);
+      } catch (error) {
+        console.log("Error Fetching data", error);
+      }
+    };
+    fetchData(); //func call
+  }, []);
+
+  //filtereing func
+  const filterItems = (category) => {
+    const filtered =
+      category === "all"
+        ? products
+        : products.filter((item) => item.category === category);
+
+    setFilteredItems(filtered);
+    setSelectedCategory(category);
+  };
+
+  //show all products
+  const showAll = () => {
+    setFilteredItems(products);
+    setSelectedCategory("all");
+  };
+
   return (
     <div className="max-w-screen-2xl container mx-auto xl:px-28 px-4 mb-12">
       <h2 className="title">Subscribe to the newsletter</h2>
@@ -11,10 +52,10 @@ export default function Products() {
         <div className="flex flex-col md:flex-row flex-wrap md:justify-between items-center space-y-3 mb-8">
           {/* all btns */}
           <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4 flex-wrap">
-            <button>All Products</button>
-            <button>T-shirts</button>
-            <button>Hoodies</button>
-            <button>Bags</button>
+            <button onClick={showAll}>All Products</button>
+            <button onClick={() => filterItems("Dress")}>T-shirts</button>
+            <button onClick={() => filterItems("Hoodies")}>Hoodies</button>
+            <button onClick={() => filterItems("Bags")}>Bags</button>
           </div>
 
           {/* Sorting options */}
@@ -31,7 +72,8 @@ export default function Products() {
             </select>
           </div>
         </div>
-        
+        {/* products data */}
+        <Cards filteredItems={filteredItems} />
       </div>
     </div>
   );
