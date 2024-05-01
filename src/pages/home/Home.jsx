9e5cf4a20d007ddfb302520d3e58409dd3af791d
Joeeazy <joeeazy34@gmail.com>
2024-05-01 13:44:34 +0300

    Collections section done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Banner from "./Banner";
 import Category from "./Category";
+import Collections from "./Collections";
 import Products from "./Products";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Banner />
       <Category />
       <Products />
+      <Collections />
     </div>
   );
 }
